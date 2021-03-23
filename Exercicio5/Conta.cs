a27b01437e4d82f79580f82bff73087e8446e998
Wallace Silva <wleitte@outlook.com>
2021-03-23 13:57:31 -0300

    Projeto corrigido de acordo com as aulas do professor

---
+++ Exercicio5/Conta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exercicio5
+{
+    class Conta
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Conta(int numero, string titular, double quantia) : this(numero, titular)
+        {
+            DepositarValor(quantia);
+        }
+
+        public void DepositarValor(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void SacarValor(double quantia)
+        {
+            double taxaSaque = 5.00;
+            Saldo -= (quantia + taxaSaque);
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c;
+
+            Console.Write("Entre o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string titularConta = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial? (s/n): ");
+            char haveraDeposito = char.Parse(Console.ReadLine().ToLower());
+            
+            if (haveraDeposito == 's')
+            {
+                Console.Write("Entre o valor do depósito: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new Conta(numeroConta, titularConta, depositoInicial);
+            }
+            else
+            {
+                c = new Conta(numeroConta, titularConta);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para depósito: ");
+            double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.DepositarValor(valorDeposito);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.SacarValor(valorSaque);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c);
+        }
+    }
+}
